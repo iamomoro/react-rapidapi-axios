67d8882384b46ce67504a7f778252ffdee40c3fa
Elvis Omoro <iamomoroit@gmail.com>
2022-11-15 23:17:34 +0300

    axios-fetch-url-headers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import DataCard from './DataCard';
 
-import './App.css';
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { usersCollection: [] };
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
- 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  componentDidMount() {
+    const options = {
+      method: 'GET',
+      url: 'https://us-states.p.rapidapi.com/basic',
+      params: { limit: '10', page: '1' },
+      headers: {
+        'X-RapidAPI-Key': 'fa0828195cmsh9f824b0cab82622p1c348ajsnc8b29806bee2',
+        'X-RapidAPI-Host': 'us-states.p.rapidapi.com'
+      },
+    };
+
+    axios
+      .request(options)
+      .then((response) => {
+        this.setState({ usersCollection: response.data });
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  }
+
+  dataCard() {
+    return this.state.usersCollection.map((data, i) => {
+      return <DataCard obj={data} key={i} />;
+    });
+  }
+
+  render() {
+    return (
+      <>
+        <div style={{ marginTop: '50px', padding: '20px' }}>
+          {this.dataTable()}
+        </div>
+      </>
+    );
+  }
 }
-
-export default App;
---
+++ src/DataCard.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class CharacterCard extends Component {
+  render() {
+    return (
+      <>
+      </>
+    );
+  }
+}
+
+export default CharacterCard;
